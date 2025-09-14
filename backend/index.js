09044d1e4100106374b50e6e6e45c371da092db1
Pavansai2205 <saipavan931@gmail.com>
2025-09-14 23:39:26 +0530

    url updated

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://hirevoo.onrender.com/',
     credentials:true
 }
-
+  
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
